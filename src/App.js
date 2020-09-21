b69a28dbe7424d66e17b629be2f7ebad32ab9dd3
Tomas Meleg <tomas.meleg@gmail.com>
2020-09-21 17:17:25 +0200

    Remove number of jobs from title

--- src/App.js
+++ src/App.js
@@ -20,7 +20,7 @@ function App() {
 
   return (
     <Container className="my-4">
-      <h1 className="mb-4"> Github jobs {jobs.length}</h1>
+      <h1 className="mb-4"> Github jobs</h1>
       <SearchForm params={params} onParamChange={handleParamChange} />
       <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading && <h1> Loading...</h1>}
